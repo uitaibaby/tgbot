22379ab6fc3d4ee97bc851f1bd172581eaeb8edc
uitaibaby <uitaibaby88@mail.ru>
2022-08-23 12:12:13 +1000

    not working version

---
+++ src/main/java/ru/skillfactorydemo/tgbot/dto/GetCursOnDateXml.java
@@ -0,0 +1,17 @@
+package ru.skillfactorydemo.tgbot.dto;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+@XmlRootElement(name = "GetCursOnDateXML", namespace = "http://web.cbr.ru/")
+@Data
+public class GetCursOnDateXml {
+
+    @XmlElement(name = "On_date", required = true, namespace = "http://web.cbr.ru/")
+    protected XMLGregorianCalendar onDate;
+}
